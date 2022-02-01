2f6440f759800f4481119ceb4a981d1f04b4c6cc
Awais Aslam <awaisaslam336@gmail.com>
2022-02-01 19:42:13 +0500

    Transforming Product Data

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -4,16 +4,12 @@ namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
-{
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
-}
+// TILL NOW THIS CODE DOES NOT MATTER
+// class ProductCollection extends ResourceCollection
+// {
+    
+//     public function toArray($request)
+//     {
+//         return parent::toArray($request);
+//     }
+// }
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -6,20 +6,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
+    
     public function toArray($request)
     {
         return [
             'name'=> $this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock == 0 ? 'Out of Stock!':$this->stock,
             'discount'=>$this->discount,
+            'totalPrice'=> round((1-($this->discount/100)) * $this->price,2),
+            'rating'=>round($this->reviews->avg('star'),2),
+            'href'=>[
+                'reviews' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
